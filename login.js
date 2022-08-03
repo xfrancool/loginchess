6aadffa25038120e7a74add6124d38a53f66e470
Franco Ortega <francoortega961@gmail.com>
2022-08-03 12:10:46 -0300

    first commit

---
+++ login.js
@@ -0,0 +1,10 @@
+function login() {
+  var user, pass;
+
+  user = document.getElementById("usuario").value;
+  pass = document.getElementById("password").value;
+
+  if (user == "elimaryarg" && pass == "hola12") {
+    window.location = "inicio.html";
+  }
+}
