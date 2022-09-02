6dc6ecc49521d1ef78999277fd44bf5ad37b70ed
Franco Ortega <francoortega961@gmail.com>
2022-09-02 20:37:39 -0300

    c1

--- login.js
+++ login.js
@@ -6,6 +6,9 @@ function login() {
 
   if (user == "Grupo1" && pass == "1516") {
     window.location = "grupos/primero/grupo1.html";
+  }
+  if (user == "Sergio29m" && pass == "5445") {
+    window.location = "grupos/sergio/sergio.html";
   } else if (user == "elimaryarg" && pass == "hola12") {
     window.location = "inicio.html";
   } else {
