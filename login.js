2ff2f51e23d0016cb256c5728d0fab85f1aae383
Franco Ortega <francoortega961@gmail.com>
2022-09-08 10:10:12 -0300

    c3

--- login.js
+++ login.js
@@ -12,6 +12,8 @@ function login() {
     window.location = "grupos/sergio/sergio.html";
   } else if (user == "EricLista" && pass == "1518") {
     window.location = "grupos/eric/eric.html";
+  } else if (user == "Mateo1" && pass == "1519") {
+    window.location = "grupos/mateo/mateo.html";
   } else if (user == "elimaryarg" && pass == "hola12") {
     window.location = "inicio.html";
   } else {
