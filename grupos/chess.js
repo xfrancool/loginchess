9934fdfa4e7cf41b1767a14df0ccad398883e526
Franco Ortega <francoortega961@gmail.com>
2022-10-05 01:38:55 -0300

    c5

---
+++ grupos/chess.js
@@ -0,0 +1,24 @@
+const chess = [
+  {
+    id: 1,
+  },
+];
+
+function Template(chess) {
+  return `
+  
+  <div class="textcard">
+      <p class="parrafocard">
+          <h3>Ejercicios de Mate</h3>
+         <a href="https://lichess.org/study/Qn7Je4hg">Click aqui</a>
+      </p>
+    </div>
+    
+    `;
+}
+
+document.getElementById("chess").innerHTML = `
+            
+            ${chess.map(Template).join("")}
+            
+          `;
