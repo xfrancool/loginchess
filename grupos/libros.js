546497b971eac3c6f5e7c3b211fe8d495d858089
Franco Ortega <francoortega961@gmail.com>
2022-10-12 14:36:26 -0300

    c3

---
+++ grupos/libros.js
@@ -0,0 +1,25 @@
+const book = [
+  {
+    id: 1,
+  },
+];
+
+function Template(book) {
+  return `
+    
+  
+          Libro PDF
+          <a  href="https://www.mediafire.com/file/40fzkxw49rad2xy/Mejore_su_t%25C3%25A1ctica_en_1001_problemas_-_Zen%25C3%25B3n_Franco.pdf/file" 
+          ><img src="../img/libro.png" class="logozoom" alt=""
+        /></a>
+        
+        
+      
+      `;
+}
+
+document.getElementById("book").innerHTML = `
+              
+              ${book.map(Template).join("")}
+              
+            `;
