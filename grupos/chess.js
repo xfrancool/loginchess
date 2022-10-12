d1612b3e6e007917a9dc3f829c0c9bb9bbfa83fa
Franco Ortega <francoortega961@gmail.com>
2022-10-12 01:52:31 -0300

    c3

--- grupos/chess.js
+++ grupos/chess.js
@@ -9,6 +9,12 @@ function Template(chess) {
   
   <div class="textcard">
       <p class="parrafocard">
+          <h3>Táctica</h3>
+         <a href="https://lichess.org/study/nQIobiZL" target="_blank">Click aqui</a>
+      </p>
+    </div>
+  <div class="textcard">
+      <p class="parrafocard">
           <h3>Ejercicios de Mate</h3>
          <a href="https://lichess.org/study/Qn7Je4hg" target="_blank">Click aqui</a>
       </p>
