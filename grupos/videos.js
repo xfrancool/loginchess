37d2263d2bd831bf3695fe5c3cb884a0de09df21
xfrancool <francoortega961@gmail.com>
2022-10-21 13:41:52 -0300

    c2

--- grupos/videos.js
+++ grupos/videos.js
@@ -8,87 +8,31 @@ function Template(videosA) {
   return `
   <h1 id="textfinal">Videos adicionales</h1>
   <div class="">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/VVHyP9sXCDw"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/cccsBeyTUPA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
   
   
   <div class="">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/sFV5n85c0mo"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/e57NqRHE6AY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
   
   <div class="">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/V716JfKf4YM"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/GdSqPHR5rAo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 
   <div class="">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/176SqKu2Ikk"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/2UeYpl46ggc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
    
-  <div class="">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/zktq2s3B_nU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </div>
  
+  
   <div class="">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/uFraLjwEOC4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </div>
-  <!--
-  <div class="">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/GMJKxT9HWpE"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/aOgDbm8dqsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
   <br />
   <br />
 
-  <div class="">
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/qyBJiGUp8Ek"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    ></iframe>
-  </div> -->
+  
     `;
 }
 
