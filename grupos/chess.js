39eeddfc945489d372234dc85a1c3a4a493d1e9e
xfrancool <francoortega961@gmail.com>
2022-10-27 01:22:58 -0300

    c1

--- grupos/chess.js
+++ grupos/chess.js
@@ -18,6 +18,16 @@ function Template(chess) {
           <h3>Ejercicios de Mate</h3>
          <a href="https://lichess.org/study/Qn7Je4hg" target="_blank">Click aqui</a>
       </p>
+      
+    </div>
+  <div class="textcard">
+      <p class="parrafocard">
+
+          <h3>Ejercicios diarios</h3>
+          <p>Ser constante, pensar antes mover son las claves para avanzar</p>
+         <a href="https://lichess.org/training/" target="_blank">Click aqui</a>
+      </p>
+
     </div>
     
     `;
