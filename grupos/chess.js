1812e5c9da64c2334d29329fa8b0f79c53943ac0
xfrancool <francoortega961@gmail.com>
2022-10-27 19:38:39 -0300

    c1

--- grupos/chess.js
+++ grupos/chess.js
@@ -8,27 +8,17 @@ function Template(chess) {
   return `
   
   <div class="textcard">
-      <p class="parrafocard">
-          <h3>Táctica</h3>
-         <a href="https://lichess.org/study/nQIobiZL" target="_blank">Click aqui</a>
-      </p>
-    </div>
-  <div class="textcard">
-      <p class="parrafocard">
-          <h3>Ejercicios de Mate</h3>
-         <a href="https://lichess.org/study/Qn7Je4hg" target="_blank">Click aqui</a>
-      </p>
-      
-    </div>
-  <div class="textcard">
-      <p class="parrafocard">
-
-          <h3>Ejercicios diarios</h3>
-          <p>Ser constante, pensar antes mover son las claves para avanzar</p>
-         <a href="https://lichess.org/training/" target="_blank">Click aqui</a>
-      </p>
-
-    </div>
+            <p class="parrafocard">
+                <h3>Sección de estudio
+                </h3>
+               <a href="estudiar.html" ><button class="learn-more" id="btn-more">   
+                <span class="circle" aria-hidden="true">
+                <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Click aquí</span> 
+                </button></a>
+            </p>
+          </div>
     
     `;
 }
