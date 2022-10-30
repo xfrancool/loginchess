5f0406ad3e3679da3b0c50aac68850dc31f1cfc7
xfrancool <francoortega961@gmail.com>
2022-10-30 10:15:57 -0300

    c1

--- grupos/chess.js
+++ grupos/chess.js
@@ -6,20 +6,6 @@ const chess = [
 
 function Template(chess) {
   return `
-  
-  <div class="textcard">
-            <p class="parrafocard">
-                <h3>Mis clases
-                </h3>
-                <p>Todas se encuentran guardadas</p>
-               <a href="clases.html" ><button class="learn-more" id="btn-more">   
-                <span class="circle" aria-hidden="true">
-                <span class="icon arrow"></span>
-                </span>
-                <span class="button-text">Click aquí</span> 
-                </button></a>
-            </p>
-          </div>
   <div class="textcard">
             <p class="parrafocard">
                 <h3>Sección de estudio
@@ -33,6 +19,21 @@ function Template(chess) {
                 </button></a>
             </p>
           </div>
+  <div class="textcard">
+  
+            <p class="parrafocard">
+                <h3>Mis clases
+                </h3>
+                <p>Todas se encuentran guardadas</p>
+               <a href="clases.html" ><button class="learn-more" id="btn-more">   
+                <span class="circle" aria-hidden="true">
+                <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Click aquí</span> 
+                </button></a>
+            </p>
+          </div>
+  
           <div class="textcard">
             <p class="parrafocard">
                 <h3>Mis videos
